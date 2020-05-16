136fe09a2221acc0e77ce7433e8da1705c62eaaf
BanGod <krapivin_2000@mail.ru>
2020-05-16 14:03:59 +0300

    Global Var

--- BotQuest/views.py
+++ BotQuest/views.py
@@ -1,13 +1,13 @@
 from models import database, User, Point
 from datetime import date
-# from config import is_logged_in
+from config import is_logged_in
 
 def create_tables():
     with database:
         database.create_tables([User, Point])
 
 def signup(username):
-    # is_logged_in = True
+    is_logged_in = True
     return User.create(username=username, join_date=date(), score = 0)
 
 def get_points():
