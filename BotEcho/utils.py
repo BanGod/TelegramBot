923ec2f42b7beaa855718cd2ea2e1343f52f3fab
BanGod <krapivin_2000@mail.ru>
2020-05-23 09:33:27 +0300

    Fix

---
+++ BotEcho/utils.py
@@ -0,0 +1,8 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types=["text"])
+def echo(message):
+	bot.send_message(message.chat.id, message.text)
